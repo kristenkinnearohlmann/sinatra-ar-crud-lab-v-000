19b1d1ca1f3e8af02213057829b67f3912928f45
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-21 17:32:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,34 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+
+    erb :'../views/index'
+  end
+
+  get '/articles/new' do
+    erb :'../views/new'
+  end
+
+  post '/articles' do
+    @article = Article.new(title: params[:title], content: params[:content])
+    @article.save
+
+    redirect "/articles/#{@article[:id]}" # need double quotes to invoke string interpolation
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+
+    erb :'../views/show'
+  end
+
+  get '/articles/:id/edit' do
+    puts "I'm here"
+    puts params[:id]
+    binding.pry
+  end
+
 end
